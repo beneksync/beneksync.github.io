a3102f3af029661ffc54a9f08d25bf22c340baca
benek <benek@benek.pl>
2022-07-17 18:53:28 +0200

    .

--- sw.js
+++ sw.js
@@ -16,9 +16,10 @@ self.addEventListener('activate', (event) => {
   console.log('V1 now ready to handle fetches!');
 })
 
-  self.addEventListener('fetch', (event) => {
-    console.log(e.request.url);
-    event.respondWith(
-      caches.match(event.request).then((response) => response || fetch(event.request)),
-    );
-  });
+self.addEventListener('fetch', (event) => {
+  console.log(event.request.url);
+  event.respondWith(
+    //caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => fetch(event.request))
+  );
+});
